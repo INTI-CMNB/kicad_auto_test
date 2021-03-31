eef6aa7b2fb33e7a5753d6c157310259db863644
Salvador E. Tropea <salvador@inti.gob.ar>
2021-03-31 15:04:38 -0300

    Removed download.sh

--- download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm *.deb
-#curl -s https://api.github.com/repos/set-soft/docopt/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
-curl -s https://api.github.com/repos/set-soft/coveralls-python/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
-
