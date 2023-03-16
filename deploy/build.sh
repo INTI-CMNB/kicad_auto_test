45976b119bd37a6126b2419600185bc0b0e4d645
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-16 13:54:05 -0300

    Added pull before tag in deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 set -e
 # KiCad 6, tag the generic ki6 as latest and also for Docker Hub
+docker pull ghcr.io/inti-cmnb/kicad_auto_test:ki6
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki6 setsoft/kicad_auto_test:ki6
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki6 ghcr.io/inti-cmnb/kicad6_auto_full:latest
 docker push setsoft/kicad_auto_test:ki6
 docker push ghcr.io/inti-cmnb/kicad6_auto_full:latest
 # KiCad 7
+docker pull ghcr.io/inti-cmnb/kicad_auto_test:ki7
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki7 setsoft/kicad_auto_test:ki7
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki7 ghcr.io/inti-cmnb/kicad7_auto_full:latest
 docker push setsoft/kicad_auto_test:ki7
 docker push ghcr.io/inti-cmnb/kicad7_auto_full:latest
 # KiCad 5
-docker tag ghcr.io/inti-cmnb/kicad_auto_test:latest setsoft/kicad_auto_test:latest
-docker tag ghcr.io/inti-cmnb/kicad_auto_test:latest ghcr.io/inti-cmnb/kicad5_auto_full:latest
+docker pull ghcr.io/inti-cmnb/kicad_auto_test:ki5
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki5 setsoft/kicad_auto_test:latest
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki5 ghcr.io/inti-cmnb/kicad5_auto_full:latest
 docker push setsoft/kicad_auto_test:latest
 docker push ghcr.io/inti-cmnb/kicad5_auto_full:latest
 
