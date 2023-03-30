0c3f076d53e582e17ee39b929f463853ce795d30
Salvador E. Tropea <salvador@inti.gob.ar>
2023-03-30 08:00:04 -0300

    Added patched dl_deb.py
    - sleep from time

--- ki7_pre/build.sh
+++ ki7_pre/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+cp ../dl_deb.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad7_auto_full:latest_deps .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest_full kicad_version.py`
 TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad7_debian:latest_full cat /etc/debian_version | tr -d '\n'`
