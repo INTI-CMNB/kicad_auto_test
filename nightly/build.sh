f01ef7f50165059c1b558736d74d9cf0e04a9479
Salvador E. Tropea <salvador@inti.gob.ar>
2023-04-07 14:27:00 -0300

    kicad_auto_full -> kicad_auto_test

--- nightly/build.sh
+++ nightly/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 cp ../enable_pcb2blender.py .
-docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_auto_full:nightly .
-TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/' | tr -d '\n'`
-TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly kicad_version.py`
-TG3=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
-TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
-docker tag ghcr.io/inti-cmnb/kicad_auto_full:nightly ghcr.io/inti-cmnb/kicad_auto_full:${TG1}_${TG2}_${TG3}_${TG4}
-docker push ghcr.io/inti-cmnb/kicad_auto_full:${TG1}_${TG2}_${TG3}_${TG4}
-docker push ghcr.io/inti-cmnb/kicad_auto_full:nightly
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_auto_test:nightly .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/' | tr -d '\n'`
+TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly kicad_version.py`
+TG3=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
+TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:nightly ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_${TG4}
+docker push ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_${TG4}
+docker push ghcr.io/inti-cmnb/kicad_auto_test:nightly
 
--- nightly_pre/build.sh
+++ nightly_pre/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps .
-TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps kicad_version.py`
-TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
-TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
-docker tag ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps ghcr.io/inti-cmnb/kicad_auto_full:${TG1}_${TG2}_${TG3}_deps
-docker push ghcr.io/inti-cmnb/kicad_auto_full:${TG1}_${TG2}_${TG3}_deps
-docker push ghcr.io/inti-cmnb/kicad_auto_full:nightly_deps
+docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps .
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps kicad_version.py`
+TG2=`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps /bin/bash -c "grep DISTRIB_CODENAME /etc/lsb-release | sed  s/DISTRIB_CODENAME=//" | tr -d '\n'`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_deps
+docker push ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_deps
+docker push ghcr.io/inti-cmnb/kicad_auto_test:nightly_deps
