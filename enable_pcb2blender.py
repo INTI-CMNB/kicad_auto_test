72904250f4d1289ddc0d4235208cb69c0d01a74c
Salvador E. Tropea <salvador@inti.gob.ar>
2023-10-11 13:00:33 -0300

    pcb2blender 2.7

--- enable_pcb2blender.py
+++ enable_pcb2blender.py
@@ -2,6 +2,6 @@ import bpy
 
 # Register the addon and enable it
 bpy.context.preferences.filepaths.script_directory = '/usr/bin/3.5/scripts/'
-bpy.ops.preferences.addon_install(filepath='./pcb2blender_importer_2-6.zip', target='PREFS')
+bpy.ops.preferences.addon_install(filepath='./pcb2blender_importer_2-7.zip', target='PREFS')
 bpy.ops.preferences.addon_enable(module='pcb2blender_importer')
 bpy.ops.wm.save_userpref()
