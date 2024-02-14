84b20712194127223ca0c72ecbe8f70e83613a56
Salvador E. Tropea <salvador@inti.gob.ar>
2024-02-14 17:09:27 -0300

    Fixed nigtly tags

--- nightly/build.sh
+++ nightly/build.sh
@@ -7,5 +7,5 @@ TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly kicad_version.py
 TG3=d`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly cat /etc/debian_version | tr -d '\n'`
 TG4=b`docker run --rm ghcr.io/inti-cmnb/kicad_auto_test:nightly /bin/bash -c "ls -d /usr/bin/?.? | tr -d '\n' | tail -c 3"`
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:nightly ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_${TG4}
-#docker push ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_${TG4}
-#docker push ghcr.io/inti-cmnb/kicad_auto_test:nightly
+docker push ghcr.io/inti-cmnb/kicad_auto_test:${TG1}_${TG2}_${TG3}_${TG4}
+docker push ghcr.io/inti-cmnb/kicad_auto_test:nightly
