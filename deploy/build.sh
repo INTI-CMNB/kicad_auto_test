30de976345ab61b1d6943b41abf0b35b16c8ca26
Salvador E. Tropea <salvador@inti.gob.ar>
2024-03-21 12:58:52 -0300

    Added KiCad 8 to the deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -12,6 +12,12 @@ docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki7 setsoft/kicad_auto_test:ki7
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki7 ghcr.io/inti-cmnb/kicad7_auto_full:latest
 docker push setsoft/kicad_auto_test:ki7
 docker push ghcr.io/inti-cmnb/kicad7_auto_full:latest
+# KiCad 8
+docker pull ghcr.io/inti-cmnb/kicad_auto_test:ki8
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki8 setsoft/kicad_auto_test:ki8
+docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki8 ghcr.io/inti-cmnb/kicad8_auto_full:latest
+docker push setsoft/kicad_auto_test:ki8
+docker push ghcr.io/inti-cmnb/kicad8_auto_full:latest
 # KiCad 5
 docker pull ghcr.io/inti-cmnb/kicad_auto_test:ki5
 docker tag ghcr.io/inti-cmnb/kicad_auto_test:ki5 setsoft/kicad_auto_test:latest
