d3983314eb2d1f368024fcf43917420f9076297a
Salvador E. Tropea <salvador@inti.gob.ar>
2025-02-21 08:31:35 -0300

    Patch for KiBot 1.8.2 frame_plotter issues

--- ki8/build.sh
+++ ki8/build.sh
@@ -2,6 +2,7 @@
 set -e
 cp ../enable_pcb2blender.py .
 cp ../materials.py .
+cp ../frame_plotter .
 . ../iteration.sh
 docker build -f Dockerfile --build-arg iteration=${IT} -t ghcr.io/inti-cmnb/kicad8_auto_full:latest .
 . ./tgs.sh
