e8b55fe323d7845721b533dadbfeb83fde59d062
Salvador E. Tropea <salvador@inti.gob.ar>
2026-04-10 13:12:39 -0300

    [Added][KiCad 10] Experimental images

---
+++ ki10/run_shell.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --rm -it --workdir=/work --volume=`pwd`:/work:rw ghcr.io/inti-cmnb/kicad_auto_test:ki10 /bin/bash
---
+++ ki10_pre/tgs.sh
@@ -0,0 +1,3 @@
+TG1=`docker run --rm ghcr.io/inti-cmnb/kicad9_auto_full:latest_deps kicad_version.py`
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad9_auto_full:latest_deps cat /etc/debian_version | tr -d '\n'`
+TG3=b`docker run --rm ghcr.io/inti-cmnb/kicad9_auto_full:latest_deps /bin/bash -c "blender --version | head -n 1 | tr -d 'Blender '"`
